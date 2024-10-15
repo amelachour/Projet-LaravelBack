5702e2aeee2b45f66b28dd4a538fbee489dc5f71
Amel Achour <amelacho3@gmail.com>
2024-10-15 17:24:29 +0100

    statistique Centre de recyclage selon catégorie

--- app/Http/Controllers/dashboard/Analytics.php
+++ app/Http/Controllers/dashboard/Analytics.php
@@ -1,14 +1,25 @@
 <?php
-
 namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\RecyclingCenter;
 
 class Analytics extends Controller
 {
-  public function index()
-  {
-    return view('content.dashboard.dashboards-analytics');
-  }
+    public function index()
+    {
+        //Centre selon Catégorie !! 
+        $categories = Category::withCount('recyclingCenters')->get();
+        $categoryNames = $categories->pluck('name');
+        $centerCounts = $categories->pluck('recycling_centers_count');
+        $totalCenters = RecyclingCenter::count(); 
+        $totalCategories = Category::count();     
+        $topCategory = $categories->sortByDesc('recycling_centers_count')->first()->name; 
+
+        
+        // Passez les variables à la vue
+        return view('content.dashboard.dashboards-analytics', compact('categoryNames', 'centerCounts', 'totalCenters', 'totalCategories', 'topCategory'));
+    }
 }
